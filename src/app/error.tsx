f06f28a028aaee0bd7ba0954a54ad1a9e55f7937
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-03-28 02:23:20 +0600

    style: modified error.tsx ui

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,28 +1,46 @@
-"use client"; // Error boundaries must be Client Components
+"use client";
 
+import { Button } from "@/components/ui/button";
+import { AlertTriangle, RotateCcw, MoveLeft } from "lucide-react";
+import Link from "next/link";
 import { useEffect } from "react";
 
 export default function Error({
-    error,
-    reset,
+  error,
+  reset,
 }: {
-    error: Error & { digest?: string };
-    reset: () => void;
+  error: Error & { digest?: string };
+  reset: () => void;
 }) {
-    useEffect(() => {
-        console.error(error);
-    }, [error]);
+  useEffect(() => {
+    console.error("App Error:", error);
+  }, [error]);
 
-    return (
-        <div>
-            <h2>Something went wrong!</h2>
-            <button
-                onClick={
-                    () => reset()
-                }
-            >
-                Try again
-            </button>
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-center p-4">
+      <h1 className="text-4xl font-bold mb-4 flex items-center gap-2">
+        <AlertTriangle className="w-12 h-12 text-destructive" />
+        Something Went Wrong
+      </h1>
+
+      <p className="mb-6 text-muted-foreground max-w-md">
+        Sorry, something unexpected happened. Please try again or go back to the
+        homepage.
+      </p>
+
+      <div className="flex gap-3">
+        <Button size="lg" variant="default" onClick={() => reset()}>
+          <RotateCcw />
+          Try Again
+        </Button>
+
+        <Link href="/">
+          <Button size="lg" variant="outline">
+            <MoveLeft />
+            Go Home
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
         <QueryProvider>
           <AuthProvider>
             {children}
-            <Toaster position="top-right" richColors />
+            <Toaster position="bottom-right" richColors />
           </AuthProvider>
         </QueryProvider>
       </body>
