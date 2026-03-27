b2f136a89b3be2f3b375b859fc4a0182327714c0
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-03-27 10:25:59 +0600

    feat: initial commit

---
+++ src/app/loading.tsx
@@ -0,0 +1,6 @@
+import { Spinner } from "@/components/ui/spinner";
+
+export default function GlobalLoading() {
+  // Or a custom loading skeleton component
+  return <Spinner/>;
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { MoveLeft, SearchX } from "lucide-react";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-center p-4">
+      <h1 className="text-4xl font-bold mb-4 flex  items-center gap-2"><SearchX className="w-12 h-12 " /> - Page Not Found</h1>
+      <p className="mb-6">Sorry, the page you are looking for does not exist.</p>
+      <Link href="/" className="">
+        <Button size={"lg"} className="text-base">
+          <MoveLeft />
+          Go Home
+        </Button>
+      </Link>
+    </div>
+  );
+}
