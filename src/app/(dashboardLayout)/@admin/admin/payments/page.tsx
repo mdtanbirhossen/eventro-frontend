d70846b448fe77624be34aaa82a932db382d6401
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-03-28 23:59:43 +0600

    feat: payments management page added

---
+++ src/app/(dashboardLayout)/@admin/admin/payments/page.tsx
@@ -0,0 +1,11 @@
+import PaymentsManagement from "@/components/modules/Admin/Payment/PaymentsManagement";
+
+const UsersListPage = async () => {
+    return (
+        <div>
+            <PaymentsManagement  />
+        </div>
+    );
+};
+
+export default UsersListPage;
