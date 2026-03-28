da8a3e8cfa3796efa071a5b22a851c7dea4fc29f
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-03-29 00:06:43 +0600

    init: initiated user dashboard

---
+++ src/app/(dashboardLayout)/@user/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+const UserDashboardPage = async () => {
+
+    return (
+        <div className="p-6 space-y-6">
+            <h1 className="text-2xl font-bold">User Dashboard</h1>
+        </div>
+    );
+};
+
+export default UserDashboardPage;
