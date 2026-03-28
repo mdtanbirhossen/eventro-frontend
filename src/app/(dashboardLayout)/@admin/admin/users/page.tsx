10ab5232e00f410da21088292b9ee089af95e9e1
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-03-28 23:59:21 +0600

    feat: users management page added

---
+++ src/app/(dashboardLayout)/@admin/admin/users/page.tsx
@@ -0,0 +1,11 @@
+import UsersManagement from "@/components/modules/Admin/User/UserManagement";
+
+const UsersListPage = async () => {
+    return (
+        <div>
+            <UsersManagement  />
+        </div>
+    );
+};
+
+export default UsersListPage;
