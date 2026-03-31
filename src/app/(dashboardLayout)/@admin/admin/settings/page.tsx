b3184559682c981ceb3d80d3f4bb4f452b070d01
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-03-31 11:10:28 +0600

    feat: added profile settings page for user and admin

---
+++ src/app/(dashboardLayout)/@admin/admin/settings/page.tsx
@@ -0,0 +1,11 @@
+import ProfileSettings from "@/components/modules/Profile/ProfileSettings";
+
+const ProfileSettingsPage = () => {
+    return (
+        <div>
+            <ProfileSettings/>
+        </div>
+    );
+}
+
+export default ProfileSettingsPage;
